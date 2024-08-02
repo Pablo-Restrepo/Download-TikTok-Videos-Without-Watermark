8b6d03f64c60cd46d391848b6480944cbd38628a
Pablo-Restrepo <restrepo736@gmail.com>
2024-08-02 02:44:09 -0500

    chore: Refactor download_tiktok_videos.py for better organization and encapsulation

--- download_tiktok_videos.py
+++ download_tiktok_videos.py
@@ -11,76 +11,78 @@ HEADERS = {
 }
 
 
-def __get_download_link(url) -> str:
-    data = {"id": url, "locale": "es", "tt": "UERKR1dk"}
-    try:
-        response = requests.post(
-            BASE_URL, data=data, headers=HEADERS, timeout=30)
+class DownloadTikTokVideos:
+    def __init__(self):
+        pass
 
-        if response.status_code != 200:
-            print(
-                f"Failed to load the page. Status code: {response.status_code}")
+    def __get_download_link(self, url) -> str:
+        data = {"id": url, "locale": "es", "tt": "UERKR1dk"}
+        try:
+            response = requests.post(
+                BASE_URL, data=data, headers=HEADERS, timeout=30)
 
-        soup = BeautifulSoup(response.content, 'html.parser')
-        link_element = soup.find('a', string='Sin marca de agua')
+            if response.status_code != 200:
+                print(f"Request failed. Status code: {response.status_code}")
 
-        if link_element:
-            return link_element.get('href')
+            soup = BeautifulSoup(response.content, 'html.parser')
+            link_element = soup.find('a', string='Sin marca de agua')
 
-        print("You tried to download another video very quickly.")
-        return None
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        return None
+            if link_element:
+                return link_element.get('href')
 
+            print("You tried to download another video very quickly.")
+            return None
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            return None
 
-def __download_video(link) -> bytes:
-    try:
-        response = requests.get(link, headers=HEADERS, timeout=70)
+    def __download_video(self, link) -> bytes:
+        try:
+            response = requests.get(link, headers=HEADERS, timeout=70)
 
-        if response.status_code == 200:
-            return response.content
+            if response.status_code == 200:
+                return response.content
 
-        print(
-            f"Failed to download. Status code: {response.status_code}")
-        return None
+            print(f"Failed to download. Status code: {response.status_code}")
+            return None
 
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        return None
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            return None
 
+    def download_video(self, url: str) -> str:
+        print(f"Downloading {url}...")
+        download_link = self.__get_download_link(url)
 
-def download_video(url: str) -> str:
-    print(f"Downloading {url}...")
-    download_link = __get_download_link(url)
+        if download_link is None:
+            print("Trying again in 10 seconds...")
+            sleep(10)
+            download_link = self.__get_download_link(url)
 
-    if download_link is None:
-        print("Trying again in 10 seconds...")
-        sleep(10)
-        download_link = __get_download_link(url)
+        if download_link is None:
+            print("Unable to get the download link.")
+            return None
 
-    if download_link is None:
-        print("Unable to get the download link.")
+        video_data = self.__download_video(download_link)
 
-    video_data = __download_video(download_link)
+        if video_data is None:
+            print("Unable to get the video.")
+            return None
 
-    if video_data is None:
-        print("Unable to get the video.")
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        video_path = os.path.join(current_dir, 'video.mp4')
 
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    video_path = os.path.join(current_dir, 'video.mp4')
+        try:
+            count = 1
+            while os.path.exists(video_path):
+                video_path = os.path.join(current_dir, f'video ({count}).mp4')
+                count += 1
 
-    try:
-        count = 1
-        while os.path.exists(video_path):
-            video_path = os.path.join(current_dir, f'video ({count}).mp4')
-            count += 1
+            with open(video_path, "wb") as file:
+                file.write(video_data)
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            return None
 
-        with open(video_path, "wb") as file:
-            file.write(video_data)
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        return None
-
-    print("Download successful.")
-    return video_path
+        print("Download successful.")
+        return video_path
---
+++ example_download_videos.py
@@ -0,0 +1,7 @@
+from download_tiktok_videos import DownloadTikTokVideos
+
+URL = "https://www.tiktok.com/@_criseditz/video/7266607615222828293"
+
+downloader = DownloadTikTokVideos()
+
+downloader.download_video(URL)
